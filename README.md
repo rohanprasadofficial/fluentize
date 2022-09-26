63b1b44ad46bd5b6d23cd03540cb904e7540b70b
Rohan Prasad <prasadro@microsoft.com>
2022-09-26 14:21:17 +0530

    initial dev

---
+++ README.md
@@ -0,0 +1,9 @@
+
+## Figma Plugin - Fluent Code Generator
+
+This will generate fluent code for selected figma design component files. 
+
+### Usage : 
+
+Decide props for React-Native , Android , iOS Platforms.
+Dynamic documentation for React Native Platforms.
---
+++ src/app/components/App.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import '../styles/ui.css';
+
+declare function require(path: string): any;
+
+const App = ({}) => {
+    // const textbox = React.useRef<HTMLInputElement>(undefined);
+
+    // const countRef = React.useCallback((element: HTMLInputElement) => {
+    //     if (element) element.value = '5';
+    //     textbox.current = element;
+    // }, []);
+
+    // const onCreate = () => {
+    //     const count = parseInt(textbox.current.value, 10);
+    //     parent.postMessage({pluginMessage: {type: 'create-rectangles', count}}, '*');
+    // };
+
+    // const onCancel = () => {
+    //     parent.postMessage({pluginMessage: {type: 'cancel'}}, '*');
+    // };
+
+    React.useEffect(() => {
+        // This is how we read messages sent from the plugin controller
+        console.log(window.location.pathname);
+
+    }, []);
+    return (
+      <div>Hello From Fluent</div>
+    );
+};
+
+
+export default App;
